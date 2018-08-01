48cf57444438306db07d2d6c796f0a82d79b8782
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-01 15:24:14 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
+// eslint-disable-next-line
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
